3b89c5a93b1b201128cf6cde7b30ba97390d6bb4
Adam Hacker <adamhack2006@gmail.com>
2025-07-13 09:13:36 -0700

    Init nuxt app and set up project

---
+++ personal-website/nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    // Replace with your repo name if using a project page, or '/' for user/organization page
+    baseURL: '/adamhacker06.github.io/'
+  }
+})
